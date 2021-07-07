e05f4f0ef3e81b6adc55373c51e1597a340a0fd8
vrtemm <fromartemm@gmail.com>
2021-07-07 15:43:49 +0300

    admin config

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8888/royalparket/admin/');
-define('HTTP_CATALOG', 'http://localhost:8888/royalparket/');
+define('HTTP_SERVER', 'http://localhost:8888/royal-parket/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/royal-parket/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8888/royalparket/admin/');
-define('HTTPS_CATALOG', 'http://localhost:8888/royalparket/');
+define('HTTPS_SERVER', 'http://localhost:8888/royal-parket/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/royal-parket/');
 
 // DIR
-define('DIR_APPLICATION', '/Applications/MAMP/htdocs/royalparket/admin/');
-define('DIR_SYSTEM', '/Applications/MAMP/htdocs/royalparket/system/');
-define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/royalparket/admin/language/');
-define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/royalparket/admin/view/template/');
-define('DIR_CONFIG', '/Applications/MAMP/htdocs/royalparket/system/config/');
-define('DIR_IMAGE', '/Applications/MAMP/htdocs/royalparket/image/');
-define('DIR_CACHE', '/Applications/MAMP/htdocs/royalparket/system/cache/');
-define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/royalparket/system/download/');
-define('DIR_UPLOAD', '/Applications/MAMP/htdocs/royalparket/system/upload/');
-define('DIR_LOGS', '/Applications/MAMP/htdocs/royalparket/system/logs/');
-define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/royalparket/system/modification/');
-define('DIR_CATALOG', '/Applications/MAMP/htdocs/royalparket/catalog/');
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/royal-parket/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/royal-parket/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/royal-parket/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/royal-parket/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/royal-parket/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/royal-parket/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/royal-parket/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/royal-parket/system/download/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/royal-parket/system/upload/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/royal-parket/system/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/royal-parket/system/modification/');
+define('DIR_CATALOG', '/Applications/MAMP/htdocs/royal-parket/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
