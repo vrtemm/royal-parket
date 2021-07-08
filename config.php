337a46637282b2209f53977e82b205bb4cec431a
vrtemm <fromartemm@gmail.com>
2021-07-08 12:52:05 +0300

    fix config

--- config.php
+++ config.php
@@ -12,18 +12,17 @@ define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/royal-parket/catalog/language/
 define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/royal-parket/catalog/view/theme/');
 define('DIR_CONFIG', '/Applications/MAMP/htdocs/royal-parket/system/config/');
 define('DIR_IMAGE', '/Applications/MAMP/htdocs/royal-parket/image/');
-define('DIR_CACHE', DIR_STORAGE . 'cache/');
-define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
-define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
-define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
-define('DIR_SESSION', DIR_STORAGE . 'session/');
-define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/royal-parket/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/royal-parket/system/download/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/royal-parket/system/upload/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/royal-parket/system/modification/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/royal-parket/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'royalpar_pixod');
+define('DB_DATABASE', 'royalpar_pixod_update');
 define('DB_PORT', '8889');
 define('DB_PREFIX', 'oc_');
