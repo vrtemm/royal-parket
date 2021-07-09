af3d41124f897aae7a1b5504667a39c1202f24bc
vrtemm <fromartemm@gmail.com>
2021-07-09 13:22:56 +0300

    config

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/royal-parket/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/royal-parket/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/royal-parket/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/royal-parket/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/royal-parket/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/royal-parket/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/royal-parket/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/royal-parket/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/royal-parket/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/royal-parket/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/royal-parket/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/royal-parket/system/storage/download/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/royal-parket/system/storage/upload/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/royal-parket/system/storage/modification/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/royal-parket/system/storage/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'royalpar_pixod_up');
+define('DB_PORT', '8889');
+define('DB_PREFIX', 'oc_');
